38913a8569f38c7522e1960be609379316f1fda7
laptop <drcyclotron@gmail.com>
2019-11-11 11:46:55 +0100

    Initial commit

---
+++ src/main/java/com/october/web/RandomIntegerController.java
@@ -0,0 +1,27 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Random;
+import java.util.stream.Stream;
+
+@RestController
+public class RandomIntegerController {
+    Logger logger = LoggerFactory.getLogger(RandomIntegerController.class);
+    @GetMapping(value = "/randomInteger", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Integer> getRandomTempature() {
+        Random r = new Random();
+        int high = 50;
+        return Flux.fromStream(Stream.generate(() -> r.nextInt(high))
+                .map(s -> String.valueOf(s))
+                .peek((msg) -> {
+                    logger.info(msg);
+                }))
+                .map(s -> Integer.valueOf(s))
+                .delayElements(Duration.ofSeconds(1));
+    }
+}
